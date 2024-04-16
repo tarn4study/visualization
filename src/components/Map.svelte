4578ae2ef3071ccb0ff96f60bbda3864d479ce87
tarn4study <tarnjarabe@gmail.com>
2024-04-16 22:25:58 +0700

    feat: create interactive map component

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -1,3 +1,50 @@
 <script>
   import * as d3 from "d3";
+  let width = 800;
+  let height = 600;
+
+  const geojsonPath =
+    "https://raw.githubusercontent.com/apisit/thailand.json/master/thailandWithName.json";
+
+  let geojson;
+  d3.json(geojsonPath).then((data) => (geojson = data));
+
+  $: projection = d3.geoMercator().fitSize([width, height], geojson);
+
+  $: pathGenerator = d3.geoPath(projection);
+
+  let provinces = [];
+  $: if (geojson)
+    provinces = geojson.features.map((feature) => {
+      return {
+        ...feature,
+        path: pathGenerator(feature),
+      };
+    });
+  $: console.log(provinces);
+  let name = null;
 </script>
+
+<p>{name}</p>
+<svg {width} {height}>
+  {#each provinces as { path, properties }}
+    <path
+      d={path}
+      class:active={name === properties.name}
+      on:mouseenter={() => (name = properties.name)}
+      role="presentation"
+    />
+  {/each}
+</svg>
+
+<style>
+  path {
+    fill: green;
+    stroke: none;
+    opacity: 0.5;
+    transition: opacity 0.4s ease-in-out;
+  }
+  path.active {
+    opacity: 1;
+  }
+</style>
