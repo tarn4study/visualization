027ae3ba2448d195b7c1634bde2e486a5048a56c
tarn4study <tarnjarabe@gmail.com>
2024-04-20 22:20:50 +0700

    feat: create bubble chart and input data

---
+++ src/components/Bubble.svelte
@@ -0,0 +1,19 @@
+<script>
+  import * as d3 from "d3";
+  import data from "../data/data";
+  let width = 800;
+  let height = 600;
+
+  const marketPath =
+    "https://raw.githubusercontent.com/tarn4study/visualization/master/src/data/bank_cap.csv";
+
+  let marketData = {};
+
+  d3.csv(marketPath).then((data) => {
+    data.forEach((d) => {
+      marketData[d.bank] = d.cap;
+    });
+  });
+</script>
+
+<svg></svg>
