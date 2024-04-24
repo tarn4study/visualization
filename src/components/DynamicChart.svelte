48d151389c068b70b62daa6f326564795c1647ab
tarn4study <tarnjarabe@gmail.com>
2024-04-24 18:35:32 +0700

    feat: create dynamic chart using svelte

---
+++ src/components/DynamicChart.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Bubble from "./Bubble.svelte";
+  import LdrMap from "./LdrMap.svelte";
+  import BubbleLoan from "./BubbleLoan.svelte";
+  let show = false;
+</script>
+
+<input type="checkbox" bind:checked={show} />
+
+{#if show}
+  <BubbleLoan />
+{:else}
+  <LdrMap />
+{/if}
