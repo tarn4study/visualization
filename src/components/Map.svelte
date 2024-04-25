074d6b8c2e9f1868261bc04d9f58e3079756d8f2
tarn4study <tarnjarabe@gmail.com>
2024-04-25 20:50:09 +0700

    update update

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -52,52 +52,75 @@
   const svg = document.getElementById("ldr");
 </script>
 
-<div id="zoom-container">
-  <div>
-    <svg id="ldr" {width} {height} viewBox="500 275 300 25">
-      {#each provinces as { path, properties }}
-        <path
-          d={path}
-          fill={colorScale(ldrDict[properties.name])}
-          class:active={name === properties.name}
-          on:mouseenter={() => (name = properties.name)}
-          role="presentation"
-        />
-      {/each}
-    </svg>
-    <svg {width}>
-      <g class="legend" transform={`translate(575, 0)`}>
-        <g transform="translate(10,0)">
-          <rect width="200" height="18" style="fill: url(#linearGradient)" />
-        </g>
-        <linearGradient id="linearGradient">
-          <stop offset="0%" stop-color={d3.interpolateRdBu(0)} />
-          <stop offset="50%" stop-color={d3.interpolateRdBu(0.5)} />
-          <stop offset="100%" stop-color={d3.interpolateRdBu(1)} />
-        </linearGradient>
-        <g class="legendLabels" font-family="sans-serif" font-size="10">
-          <svg>
-            <g transform="translate(10,0)">
-              {#each d3.scaleLinear().domain([0, 200]).ticks(2) as tick}
-                <text x={tick} y="30" dx=".3em" text-anchor="end">
-                  {tick}
-                </text>
-              {/each}
-            </g>
-          </svg>
-        </g>
+<div style="margin-top: -7vh;">
+  <svg id="ldr" {width} {height} viewBox="500 275 300 25">
+    {#each provinces as { path, properties }}
+      <path
+        d={path}
+        fill={colorScale(ldrDict[properties.name])}
+        class:active={name === properties.name}
+        on:mouseenter={() => (name = properties.name)}
+        role="presentation"
+      />
+    {/each}
+    <g transform="translate(636,296)"><text font-size="10">ใกล้กัน?</text></g>
+    <g transform="translate(636,300)"
+      ><text font-size="3">สีแดง = ฝากมากกว่ากู้</text></g
+    >
+    <g transform="translate(636,304)"
+      ><text font-size="3">สีน้ำเงิน = กู้มากกว่าฝาก</text></g
+    >
+    <g class="legend" transform={`translate(665, 325)`}>
+      <g transform="translate(10,0)">
+        <rect width="20" height="5" style="fill: url(#linearGradient)" />
       </g>
-    </svg>
-    <p>แผนที่แสดงอัตราส่วนสินเชื่อต่อเงินฝาก</p>
-  </div>
+      <linearGradient id="linearGradient">
+        <stop offset="0%" stop-color={d3.interpolateRdBu(0)} />
+        <stop offset="50%" stop-color={d3.interpolateRdBu(0.5)} />
+        <stop offset="100%" stop-color={d3.interpolateRdBu(1)} />
+      </linearGradient>
+      <g class="legendLabels" font-family="sans-serif" font-size="3">
+        <svg>
+          <g transform="translate(0,-12)">
+            <text x="10" y="20" dx=".3em" text-anchor="end"> 0 </text>
+            <text x="21" y="20" dx=".3em" text-anchor="end"> 100 </text>
+            <text x="32" y="20" dx=".3em" text-anchor="end"> 200 </text>
+          </g>
+        </svg>
+      </g>
+      <g font-size="3"
+        ><text x="45" y="12" dx=".3em" text-anchor="end">
+          แผนที่แสดงอัตราส่วนสินเชื่อต่อเงินฝาก
+        </text></g
+      >
+    </g>
+  </svg>
+  <!--<svg {width}>
+    <g class="legend" transform={`translate(575, 0)`}>
+      <g transform="translate(10,0)">
+        <rect width="200" height="18" style="fill: url(#linearGradient)" />
+      </g>
+      <linearGradient id="linearGradient">
+        <stop offset="0%" stop-color={d3.interpolateRdBu(0)} />
+        <stop offset="50%" stop-color={d3.interpolateRdBu(0.5)} />
+        <stop offset="100%" stop-color={d3.interpolateRdBu(1)} />
+      </linearGradient>
+      <g class="legendLabels" font-family="sans-serif" font-size="10">
+        <svg>
+          <g transform="translate(10,0)">
+            {#each d3.scaleLinear().domain([0, 200]).ticks(2) as tick}
+              <text x={tick} y="30" dx=".3em" text-anchor="end">
+                {tick}
+              </text>
+            {/each}
+          </g>
+        </svg>
+      </g>
+    </g>
+  </svg>-->
 </div>
 
 <style>
-  #zoom-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
   path {
     stroke: none;
     opacity: 1;
@@ -108,10 +131,5 @@
   }
   #ldr {
     transform: scale(1);
-  }
-  p {
-    margin-top: -8%;
-    margin-bottom: 7%;
-    text-align: center;
   }
 </style>
